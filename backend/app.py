3d181f16aee79479a0b3198af9c27eb119dafafe
Ruby-Rubin <rubinkanna.s@gmail.com>
2026-06-24 17:11:22 +0530

    added /checkout to cart.py

--- backend/app.py
+++ backend/app.py
@@ -13,12 +13,7 @@ from routes.cart import cart_bp
 Base.metadata.create_all(bind=engine)
 app = Flask(__name__)
 CORS(app)    
-prefix_map = {
-    "Breakfast": "B",
-    "Lunch": "L",
-    "Snacks": "S",
-    "Dinner": "D"
-}
+
 app.register_blueprint(sessions_bp)
 app.register_blueprint(menu_bp)
 app.register_blueprint(users_bp)
@@ -29,10 +24,6 @@ def home():
     return {
         "message": "College Canteen API Running"
     }
-
-
-
-
 
 @app.route("/admin/dashboard")
 def admin_dashboard():
@@ -51,123 +42,6 @@ def admin_dashboard():
     }
 
 
-
-
-
-
-@app.route("/checkout", methods=["POST"])
-def checkout():
-
-    data = request.get_json()
-
-    student_id = data.get("student_id")
-    meal_session_id = data.get("meal_session_id")
-    payment_method = data.get("payment_method")
-
-    db = SessionLocal()
-    
-    student = db.query(User).filter(
-        User.user_id == student_id
-    ).first()
-
-    if not student:
-        return {
-            "success": False,
-            "message": "Student not found"
-        }, 404
-    
-    if student.role != "student":
-        return {
-        "success": False,
-        "message": "Only students can place orders"
-    }, 403
-
-    session = db.query(MealSession).filter(
-        MealSession.session_id == meal_session_id
-    ).first()
-
-    if not session:
-        return {
-            "success": False,
-            "message": "Meal session not found"
-        }, 404
-    cart = db.query(Cart).filter(
-        Cart.student_id == student_id,
-        Cart.meal_session_id == meal_session_id
-    ).first()
-
-    if not cart:
-        return {
-            "success": False,
-            "message": "Cart not found"
-        }, 404
-    cart_items = db.query(CartItem).filter(
-        CartItem.cart_id == cart.cart_id
-    ).all()
-
-    if not cart_items:
-        return {
-            "success": False,
-            "message": "Cart is empty"
-        }, 400
-    total_amount = 0
-
-    for item in cart_items:
-
-        menu_item = db.query(MenuItem).filter(
-            MenuItem.menu_item_id == item.menu_item_id
-        ).first()
-
-        total_amount += (
-            float(menu_item.price)
-            * item.quantity
-        )
-    order_count = db.query(Order).count() + 1
-
-    prefix = prefix_map.get(
-        session.name,
-        "X"
-    )
-
-    token_number = f"{prefix}-{order_count}"
-    order = Order(
-        student_id=student_id,
-        meal_session_id=meal_session_id,
-        token_number=token_number,
-        order_type="PREORDER",
-        payment_method=payment_method,
-        total_amount=total_amount
-    )
-
-    db.add(order)
-    db.commit()
-    for item in cart_items:
-
-        menu_item = db.query(MenuItem).filter(
-            MenuItem.menu_item_id == item.menu_item_id
-        ).first()
-
-        order_item = OrderItem(
-            order_id=order.order_id,
-            menu_item_id=item.menu_item_id,
-            quantity=item.quantity,
-            price_at_purchase=float(menu_item.price)
-        )
-
-        db.add(order_item)
-    db.commit()
-    for item in cart_items:
-        db.delete(item)
-
-    db.delete(cart)
-    
-    db.commit()
-    return {
-    "success": True,
-    "order_id": order.order_id,
-    "token_number": token_number,
-    "total_amount": total_amount
-}
 
 if __name__ == "__main__":
     app.run(
--- backend/routes/cart.py
+++ backend/routes/cart.py
@@ -1,8 +1,15 @@
 from flask import Blueprint, request
 from database import SessionLocal
-from models import User, Cart, CartItem, MealSession, MenuItem
+from models import Order, OrderItem, User, Cart, CartItem, MealSession, MenuItem
 
 cart_bp = Blueprint("cart", __name__)
+
+prefix_map = {
+    "Breakfast": "B",
+    "Lunch": "L",
+    "Snacks": "S",
+    "Dinner": "D"
+}
 
 @cart_bp.route("/cart/session/<int:student_id>/<int:meal_session_id>", methods=["GET"])
 def get_session_cart(student_id, meal_session_id):
@@ -173,3 +180,116 @@ def create_session_cart():
         "cart_id": cart.cart_id
     }
 
+@cart_bp.route("/checkout", methods=["POST"])
+def checkout():
+
+    data = request.get_json()
+
+    student_id = data.get("student_id")
+    meal_session_id = data.get("meal_session_id")
+    payment_method = data.get("payment_method")
+
+    db = SessionLocal()
+    
+    student = db.query(User).filter(
+        User.user_id == student_id
+    ).first()
+
+    if not student:
+        return {
+            "success": False,
+            "message": "Student not found"
+        }, 404
+    
+    if student.role != "student":
+        return {
+        "success": False,
+        "message": "Only students can place orders"
+    }, 403
+
+    session = db.query(MealSession).filter(
+        MealSession.session_id == meal_session_id
+    ).first()
+
+    if not session:
+        return {
+            "success": False,
+            "message": "Meal session not found"
+        }, 404
+    cart = db.query(Cart).filter(
+        Cart.student_id == student_id,
+        Cart.meal_session_id == meal_session_id
+    ).first()
+
+    if not cart:
+        return {
+            "success": False,
+            "message": "Cart not found"
+        }, 404
+    cart_items = db.query(CartItem).filter(
+        CartItem.cart_id == cart.cart_id
+    ).all()
+
+    if not cart_items:
+        return {
+            "success": False,
+            "message": "Cart is empty"
+        }, 400
+    total_amount = 0
+
+    for item in cart_items:
+
+        menu_item = db.query(MenuItem).filter(
+            MenuItem.menu_item_id == item.menu_item_id
+        ).first()
+
+        total_amount += (
+            float(menu_item.price)
+            * item.quantity
+        )
+    order_count = db.query(Order).count() + 1
+
+    prefix = prefix_map.get(
+        session.name,
+        "X"
+    )
+
+    token_number = f"{prefix}-{order_count}"
+    order = Order(
+        student_id=student_id,
+        meal_session_id=meal_session_id,
+        token_number=token_number,
+        order_type="PREORDER",
+        payment_method=payment_method,
+        total_amount=total_amount
+    )
+
+    db.add(order)
+    db.commit()
+    for item in cart_items:
+
+        menu_item = db.query(MenuItem).filter(
+            MenuItem.menu_item_id == item.menu_item_id
+        ).first()
+
+        order_item = OrderItem(
+            order_id=order.order_id,
+            menu_item_id=item.menu_item_id,
+            quantity=item.quantity,
+            price_at_purchase=float(menu_item.price)
+        )
+
+        db.add(order_item)
+    db.commit()
+    for item in cart_items:
+        db.delete(item)
+
+    db.delete(cart)
+    
+    db.commit()
+    return {
+    "success": True,
+    "order_id": order.order_id,
+    "token_number": token_number,
+    "total_amount": total_amount
+}
