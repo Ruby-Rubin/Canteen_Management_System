13ccd7d41ebef82cf7e9568a9ba52703ca34dcf9
GeethaPrasad <geethaprasad2005@gmail.com>
2026-07-01 20:15:09 +0530

    frontend started

---
+++ canteen-frontend/src/pages/Login.jsx
@@ -0,0 +1,27 @@
+function Login() {
+    return (
+        <div>
+            <h1>College Canteen System</h1>
+
+            <h2>Student Login</h2>
+
+            <input
+                type="text"
+                placeholder="Register Number"
+            />
+
+            <br /><br />
+
+            <input
+                type="password"
+                placeholder="Password"
+            />
+
+            <br /><br />
+
+            <button>Login</button>
+        </div>
+    );
+}
+
+export default Login;
