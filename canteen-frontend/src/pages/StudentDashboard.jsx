dd18579b7f9d87f11d3cdd6ec973e8a91f3cb933
Ruby-Rubin <rubinkanna.s@gmail.com>
2026-07-05 23:31:36 +0530

    adding dynamic data in studentdashboard

--- canteen-frontend/src/pages/StudentDashboard.jsx
+++ canteen-frontend/src/pages/StudentDashboard.jsx
@@ -1,16 +1,24 @@
 import { useLocation } from "react-router-dom";
+import {useEffect, useState} from "react";
 import "./StudentDashboard.css";
+import axios from "axios";
 
 function StudentDashboard() {
+    useEffect(() => { const  response=await  axios.get("http:///orders/preorders")
+
+}, []);
+
 
     const location = useLocation();
+    const [activeSession, setActiveSession] = useState(null);
     const user = location.state.user;
-
+    setActiveSession(response.data.active_session);
+    
     return (
         <div className='student-dashboard'>
             <div className="dashboard-card">
             <h1>Welcome, {user.name} 👋</h1>
-            <p className="meal-active">Lunch is Active</p>
+            <p className="meal-active">{activeSession}</p>
             <button className="order-now">Order Now</button>
             <button className='pre-order'>Pre-Order</button>
             <button className='cart'>Cart</button>
