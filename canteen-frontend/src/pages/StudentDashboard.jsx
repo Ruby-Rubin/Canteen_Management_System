a3dbfb4e96fcc7b07001da1298d7c1c66bd84690
GeethaPrasad <geethaprasad2005@gmail.com>
2026-07-05 19:06:43 +0530

    Polished student-dashboard

--- canteen-frontend/src/pages/StudentDashboard.jsx
+++ canteen-frontend/src/pages/StudentDashboard.jsx
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom";
+import "./StudentDashboard.css";
 
 function StudentDashboard() {
 
@@ -6,12 +7,15 @@ function StudentDashboard() {
     const user = location.state.user;
 
     return (
-        <div>
+        <div className='student-dashboard'>
+            <div className="dashboard-card">
             <h1>Welcome, {user.name} 👋</h1>
-
-            <p>Register Number: {user.register_no}</p>
-
-            <p>Role: {user.role}</p>
+            <p className="meal-active">Lunch is Active</p>
+            <button className="order-now">Order Now</button>
+            <button className='pre-order'>Pre-Order</button>
+            <button className='cart'>Cart</button>
+            <button className='my-orders'>My Orders</button>
+            </div>
         </div>
     );
 }
