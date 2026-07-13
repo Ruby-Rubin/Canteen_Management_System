e56e5d7442c73013a6e2808b26d571c6c487502f
Ruby-Rubin <rubinkanna.s@gmail.com>
2026-07-13 10:06:42 +0530

    Add pre-order functionality to StudentDashboard

--- canteen-frontend/src/pages/StudentDashboard.jsx
+++ canteen-frontend/src/pages/StudentDashboard.jsx
@@ -8,15 +8,17 @@ function StudentDashboard() {
 const location = useLocation();
 const [activeSession, setActiveSession] = useState(null);
 const [futureSessions, setFutureSessions] = useState([]);
+const [showPreOrderModal, setShowPreOrderModal] = useState(false);
 const user = location.state.user;
    useEffect(() => {
+
 
     async function loadDashboard() {
 
        const response = await axios.get(
     "http://127.0.0.1:5000/orders/dashboard"
 );
-     setFutureSessions(response.data.future_sessions);
+    setFutureSessions(response.data.future_sessions);
     setActiveSession(response.data.active_session);
     }
    
@@ -33,8 +35,39 @@ const user = location.state.user;
             <div className="dashboard-card">
             <h1>Welcome, {user.name} 👋</h1>
             <p className="meal-active">{activeSession ? `${activeSession.meal_name} is Active` : "No active session"}</p>
-            <button className="order-now">Order Now</button>
-            <button className='pre-order'>Pre-Order</button>
+            <button className="order-now">Order Now</button>    
+            <button
+    className="pre-order"
+    onClick={() => setShowPreOrderModal(true)}
+>
+    Pre-Order
+</button>
+{showPreOrderModal && (
+
+    <div className="modal-overlay">
+        <div className="modal">
+        {futureSessions.map(session => (
+
+           <button key={session.session_id}>
+
+    <p>{session.meal_name}</p>
+
+    <p>
+        {session.start_time} - {session.end_time}
+    </p>
+
+</button>
+        ))}
+        <button
+    onClick={() => setShowPreOrderModal(false)}
+>
+    Cancel
+</button>
+
+    </div>
+    </div>
+
+)}
             <button className='cart'>Cart</button>
             <button className='my-orders'>My Orders</button>
             </div>
